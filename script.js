29e0dbc37afebfae087e9a9a22475969d1d1cca5
luciabertino2003 <166661272+luciabertino2003@users.noreply.github.com>
2024-06-03 18:53:34 -0300

    imagenes

--- script.js
+++ script.js
@@ -12,17 +12,3 @@ function saludo() {
 
 saludo();
 
-document.getElementById('whatsappButton').addEventListener('click', function() {
-    const phoneNumber = '1234567890'; 
-    const message = 'Hola, me gustaría obtener más información.'; 
-    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    
-    window.open(whatsappUrl, '_blank');
-}); 
-
-window.addEventListener('beforeunload', function(event) {
-    const message = '¿Estás seguro de que quieres salir?';
-    event.preventDefault();  
-    event.returnValue = message; 
-    return message;
-});
